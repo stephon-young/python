df992cef824536a6b35fa1d6bd43ceafc6220d61
stephon-young <stephon_young@163.com>
2019-12-20 11:39:13 +0800

    completed 02 day

--- 100d/02/ex/leap.py
+++ 100d/02/ex/leap.py
@@ -9,5 +9,8 @@ def is_leap_year(year):
   return False
 
 year = int(input('请输入年份: '))
+# python不支持 二元运算符 (?:)
+# val = (is_leap_year(year) == 1) ? '是' : '否'
+# print('%d 是否为闰年: %s' % (year, val))
 print('%d 是否为闰年: %d' % (year, is_leap_year(year)))
 
